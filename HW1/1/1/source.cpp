0e54732b1f3d62f1f80795f5c639ff7125a8468b
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-09-17 21:08:06 +0900

    create

---
+++ HW1/1/1/source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void main()
+{
+	string s1 = "This is ";
+	string s2 = "a C++ classroom";
+	string fullString = s1 + s2;
+	cout << fullString << endl;
+}
