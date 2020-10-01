6cb8f219ca1b6956f6753c3ba50db80afca5fbd3
Harim Jeong <harim97714@naver.com>
2020-10-01 21:10:35 +0900

    Update main.cpp

--- 200930/1/main.cpp
+++ 200930/1/main.cpp
@@ -1,11 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <time.h>
+
 using namespace std;
 int main()
 {
 	time_t now = time(NULL);
-	struct tm *lt = localtime(&now);
+	struct tm* lt = localtime(&now);
 
 	cout << lt->tm_year + 1900 << "년 " << lt->tm_mon + 1 << "월 " << lt->tm_mday << "일 " << lt->tm_hour << "시 " << lt->tm_min << "분 " << lt->tm_sec << "초" << endl;
 
