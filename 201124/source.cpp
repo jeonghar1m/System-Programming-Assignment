f6e7801d67477f9d4f90510fd5791aefe1021775
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-12-04 01:16:21 +0900

    Update source.cpp

--- 201124/source.cpp
+++ 201124/source.cpp
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <windows.h>
 #include <tchar.h>
+#include <process.h>
 using namespace std;
 
 #define NUMBER_AMOUNT 30
@@ -11,6 +12,8 @@ using namespace std;
 
 ofstream fout;
 ifstream fin;
+
+HANDLE hSemaphore;
 
 DWORD WINAPI CalculateProc(LPVOID lpParam)
 {
@@ -25,25 +28,21 @@ DWORD WINAPI CalculateProc(LPVOID lpParam)
 	return total;
 }
 
-DWORD WINAPI InputProc(LPVOID lpParam)
+DWORD WINAPI OutputProc(LPVOID lpParam)
 {
 	DWORD* nPtr = (DWORD*)lpParam;
-	DWORD no[10] = {};
+	DWORD no = 0;
 
-	for (DWORD i = 0; i < 10; i++)
-	{
-		printf("%d번째 값: ", i + 1);
-		scanf("%d", no[i]);
-		*(nPtr + i) = no[i];
-	}
+	no = *nPtr;
 
-	return 0;
+	return no;
 }
 
 int _tmain(int argc, TCHAR* argv[])
 {
 	DWORD dwThreadID[3];
 	HANDLE hThread[3];
+	HANDLE hThreadOutput[NUMBER_AMOUNT];
 
 	DWORD paramThread[30] = {};
 	DWORD total = 0;
@@ -51,6 +50,18 @@ int _tmain(int argc, TCHAR* argv[])
 	DWORD result = 0;
 
 	int select = 0;
+
+	// 세마포어 생성.
+	hSemaphore = CreateSemaphore(
+		NULL,    // 디폴트 보안관리자.
+		NUMBER_AMOUNT,      // 세마포어 초기 값.
+		NUMBER_AMOUNT,      // 세마포어 최대 값.
+		NULL     // unnamed 세마포어 구성.
+	);
+	if (hSemaphore == NULL)
+	{
+		_tprintf(_T("CreateSemaphore error: %d\n"), GetLastError());
+	}
 
 	cout << "메뉴를 선택하세요. 1. 입력 후 내보냄, 2. 불러오기" << endl;
 	cout << "입력: ";
@@ -61,55 +72,51 @@ int _tmain(int argc, TCHAR* argv[])
 	case 1:
 	{
 		//값 입력
-		//printf("10개의 값을 입력하시오.\n");
-		//for (int i = 0; i < NUMBER_AMOUNT; i++)
-		//{
-		//	printf("%d번째 값: ", i + 1);
-		//	scanf("%d", &paramThread[i]);
-		//}
-
-		hThread[0] =
-			CreateThread(
-				NULL, 0,
-				InputProc,
-				(LPVOID)(&paramThread[0]),
-				0, &dwThreadID[0]
-			);
-
-		hThread[1] =
-			CreateThread(
-				NULL, 0,
-				InputProc,
-				(LPVOID)(&paramThread[10]),
-				0, &dwThreadID[1]
-			);
-
-		hThread[2] =
-			CreateThread(
-				NULL, 0,
-				InputProc,
-				(LPVOID)(&paramThread[20]),
-				0, &dwThreadID[2]
-			);
+		printf("30개의 값을 입력하시오.\n");
+		for (int i = 0; i < NUMBER_AMOUNT; i++)
+		{
+			printf("%d번째 값: ", i + 1);
+			scanf("%d", &paramThread[i]);
+		}
 
 		fout.open("data.dat");
 
-		//입력한 숫자들 파일로 내보냄.
 		for (int i = 0; i < NUMBER_AMOUNT; i++)
-			fout << paramThread[i] << endl;
-
-		fout.close();
-
-
-		if (hThread[0] == NULL || hThread[1] == NULL || hThread[2] == NULL)
 		{
-			_tprintf(_T("Thread creation fault! \n"));
-			return -1;
+			hThreadOutput[i] =
+				CreateThread(
+				NULL, 0,
+				OutputProc,
+				(LPVOID)(&paramThread[i]),
+				0, 0
+			);
+
+			if (hThreadOutput[i] == NULL)
+			{
+				_tprintf(_T("Thread creation fault! \n"));
+				return -1;
+			}
 		}
 
-		CloseHandle(hThread[0]);
-		CloseHandle(hThread[1]);
-		CloseHandle(hThread[2]);
+		for (int i = 0; i < NUMBER_AMOUNT; i++)
+		{
+			ResumeThread(hThreadOutput[i]);
+		}
+
+		WaitForMultipleObjects(3, hThreadOutput, TRUE, INFINITE);
+
+		for (int i = 0; i < NUMBER_AMOUNT; i++)
+		{
+			GetExitCodeThread(hThreadOutput[i], &result);
+			fout << result << endl;
+		}
+
+		for (int i = 0; i < NUMBER_AMOUNT; i++)
+			CloseHandle(hThreadOutput[i]);
+
+		CloseHandle(hSemaphore);
+
+		fout.close();
 
 		break;
 	}
